65f476cd69889cc5c611baea21babb0fe44342d4
Alex Arseneau <alexander.arseneau@gmail.com>
2023-02-11 12:05:24 -0500

    Configure Keychron Q6 the way I like it: Caps Word Enabled Replace MacOS layer with Windows layer. Add reset button to layers 3 and 4 Make symbol buttons One-Shot Layer modifiers. Show NumPad as RED with num lock is disabled

--- keyboards/keychron/q6/ansi/keymaps/keychron/rules.mk
+++ keyboards/keychron/q6/ansi/keymaps/keychron/rules.mk
@@ -1,4 +1,4 @@
 VIA_ENABLE = yes
-
+CAPS_WORD_ENABLE = yes
 VPATH += keyboards/keychron/common
 SRC += keychron_common.c
--- keyboards/keychron/q6/q6.c
+++ keyboards/keychron/q6/q6.c
@@ -25,8 +25,37 @@ const matrix_row_t matrix_mask[] = {
     0b11111111111111101111,
 };
 
-#ifdef DIP_SWITCH_ENABLE
+void keyboard_post_init_user(void) {
+    rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+    rgb_matrix_sethsv_noeeprom(HSV_TEAL);
+}
 
+#if defined(CAPS_WORD_ENABLE)
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+        case KC_MINS:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+            return true;
+        case KC_LSFT:
+            add_weak_mods(MOD_BIT(KC_LSFT));
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
+#endif /* CAPS_WORD_ENABLE*/
+
+#ifdef DIP_SWITCH_ENABLE
 bool dip_switch_update_kb(uint8_t index, bool active) {
     if (!dip_switch_update_user(index, active)) {
         return false;
@@ -67,10 +96,31 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
+#if defined(NUM_LOCK_LED_INDEX)
+const uint8_t num_keys[] = NUM_KEYS_GRID;
+#endif // NUM_LOCK_LED_INDEX
+
 bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
     if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) {
         return false;
     }
+
+    if (IS_LAYER_ON(3)) {
+        RGB_MATRIX_INDICATOR_SET_COLOR(19 ,255, 0, 0);
+    } else if (IS_LAYER_ON(2)) {
+        RGB_MATRIX_INDICATOR_SET_COLOR(18 ,255, 0, 0);
+    } else if (IS_LAYER_ON(1)) {
+        RGB_MATRIX_INDICATOR_SET_COLOR(17 ,255, 0, 0);
+    } else if (IS_LAYER_ON(0)) {
+        RGB_MATRIX_INDICATOR_SET_COLOR(16 ,255, 0, 0);
+    } else
+    {
+        RGB_MATRIX_INDICATOR_SET_COLOR(16 ,0 , 0, 0);
+        RGB_MATRIX_INDICATOR_SET_COLOR(17 ,0, 0, 0);
+        RGB_MATRIX_INDICATOR_SET_COLOR(18 ,0, 0, 0);
+        RGB_MATRIX_INDICATOR_SET_COLOR(19 ,0, 0, 0);
+    }
+
     // RGB_MATRIX_INDICATOR_SET_COLOR(index, red, green, blue);
 #    if defined(CAPS_LOCK_LED_INDEX)
     if (host_keyboard_led_state().caps_lock) {
@@ -82,11 +132,13 @@ bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
     }
 #    endif // CAPS_LOCK_LED_INDEX
 #    if defined(NUM_LOCK_LED_INDEX)
-    if (host_keyboard_led_state().num_lock) {
-        RGB_MATRIX_INDICATOR_SET_COLOR(NUM_LOCK_LED_INDEX, 255, 255, 255);
+    if (!host_keyboard_led_state().num_lock) {
+        for (uint8_t i = 0; i < NUM_KEYS_GRID_LEN; i++)
+            RGB_MATRIX_INDICATOR_SET_COLOR(num_keys[i], 255, 0, 0);
     } else {
         if (!rgb_matrix_get_flags()) {
-            RGB_MATRIX_INDICATOR_SET_COLOR(NUM_LOCK_LED_INDEX, 0, 0, 0);
+            for (uint8_t i = 0; i < NUM_KEYS_GRID_LEN; i++)
+                RGB_MATRIX_INDICATOR_SET_COLOR(NUM_LOCK_LED_INDEX, 0, 0, 0);
         }
     }
 #    endif // NUM_LOCK_LED_INDEX
