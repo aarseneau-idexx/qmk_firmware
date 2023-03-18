44559b0788d786f636fbf2e0890ec140437c1144
Alex Arseneau <alexander.arseneau@gmail.com>
2023-03-18 19:21:30 -0400

    Basic Macropad Support

---
+++ keyboards/macropad/pi60/config.h
@@ -0,0 +1,7 @@
+// Copyright 2022 ziptyze (@ziptyze)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+#define NUM_ENCODER 1
---
+++ keyboards/macropad/pi60/keymaps/default/keymap.c
@@ -0,0 +1,54 @@
+/* Copyright 2022 ziptyze
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [0] = LAYOUT_20_ansi(
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+        KC_6,    KC_7,    KC_8,    KC_9,    KC_A,
+        KC_B,    KC_C,    KC_D,    KC_E,    KC_F,
+        KC_G,    KC_H,    KC_I,    KC_J,    KC_K
+    ),
+
+    [0] = LAYOUT_20_ansi(
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+        KC_6,    KC_7,    KC_8,    KC_9,    KC_A,
+        KC_B,    KC_C,    KC_D,    KC_E,    KC_F,
+        KC_G,    KC_H,    KC_I,    KC_J,    KC_K
+    ),
+
+    [0] = LAYOUT_20_ansi(
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+        KC_6,    KC_7,    KC_8,    KC_9,    KC_A,
+        KC_B,    KC_C,    KC_D,    KC_E,    KC_F,
+        KC_G,    KC_H,    KC_I,    KC_J,    KC_K
+    ),
+
+    [0] = LAYOUT_20_ansi(
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+        KC_6,    KC_7,    KC_8,    KC_9,    KC_A,
+        KC_B,    KC_C,    KC_D,    KC_E,    KC_F,
+        KC_G,    KC_H,    KC_I,    KC_J,    KC_K
+    ),
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] =  { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), },
+    };
+#endif
