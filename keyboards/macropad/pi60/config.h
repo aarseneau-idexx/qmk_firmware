3a93232ec4c22cf003c656f0c30069159271a1cb
Alex Arseneau <alexander.arseneau@gmail.com>
2023-03-28 06:39:46 -0400

    Update

--- keyboards/macropad/pi60/config.h
+++ keyboards/macropad/pi60/config.h
@@ -10,3 +10,7 @@
 #define I2C_DRIVER I2CD1
 #define I2C1_SDA_PIN GP27
 #define I2C1_SCL_PIN GP26
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U // Timeout window in ms in which the double tap can occur.
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED 25 // Specify a optional status led by GPIO number which blinks when entering the bootloader
--- keyboards/macropad/pi60/keymaps/default/rules.mk
+++ keyboards/macropad/pi60/keymaps/default/rules.mk
@@ -3,6 +3,8 @@ BOOTLOADER = rp2040
 
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306
+OCEAN_DREAM_ENABLE = no
+WPM_ENABLE = yes
 
 OPT_DEFS += -DHAL_USE_I2C=TRUE
 
@@ -11,3 +13,16 @@ LTO_ENABLE = yes
 OLED_ENABLE = yes
 
 NKRO_ENABLE = yes
+
+ifeq ($(strip $(OLED_ENABLE)), yes)
+    ifdef OCEAN_DREAM_ENABLE
+        ifeq ($(strip $(OCEAN_DREAM_ENABLE)), yes)
+            SRC += ocean_dream.c
+            OPT_DEFS += -DOCEAN_DREAM_ENABLE
+        endif
+    endif
+    ifndef OCEAN_DREAM_ENABLE
+        SRC += ocean_dream.c
+        OPT_DEFS += -DOCEAN_DREAM_ENABLE
+    endif
+endif
